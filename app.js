a9ebcca8a575e067fccea1b316fc6fc3d72d3541
Yuval Lavi <yuvallavi249@gmail.com>
2024-12-28 12:25:12 +0200

    Fix up project structure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 
 const mongoose = require("mongoose");
@@ -19,6 +19,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const posts_routes = require("./routes/posts_routes");
 app.use("/posts", posts_routes);
+
+const comments_routes = require("./routes/comments_routes");
+app.use("/comments", comments_routes);
 
 app.listen(port, () => {
   console.log("Server started");
